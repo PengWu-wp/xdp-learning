c624af524a0db4f01aecf53237655f054aa5c032
PengWu-wp <523972511@qq.com>
2021-09-29 05:36:20 -0700

    update xdp-drop-kern.c

--- basic01-load-first-xdp-program/xdp-drop-kern.c
+++ basic01-load-first-xdp-program/xdp-drop-kern.c
@@ -2,16 +2,19 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+/* SEC is a helper macro to place programs, maps, license
+ * in different sections in elf_bpf file. It is defined in
+ * bpf_helpers.h
+ */
 SEC("xdp")
 int  xdp_prog(struct xdp_md *ctx)
 {
-	char fmt[] = "Hello, XDP and eBPF!\n";
-	/* This helper function would print fmt to 
-     * /sys/kernel/debug/tracing/trace_pipe
-     */
-	bpf_trace_printk(fmt, sizeof(fmt));					      
+	/* This is a helper macro defined in bpf_helpers.h. It would
+         * print fmt to /sys/kernel/debug/tracing/trace_pipe
+         */
+	bpf_printk("Hello, XDP and eBPF!\n");					      
 					    
 	return XDP_DROP; /* Drop all packets */
 }
-/* Linux adopt GPL so it will refuse progs using other licenses */
+/* Linux kernel will refuse XDP progs using other licenses */
 char _license[] SEC("license") = "GPL";
