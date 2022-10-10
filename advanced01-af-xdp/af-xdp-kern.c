09c5f9ecaeaaa52b8fcb3eb02b5b1dac4a352a6b
PengWu <wupeng5828@163.com>
2022-10-10 07:00:33 -0700

    Update af-xdp

--- advanced01-af-xdp/af-xdp-kern.c
+++ advanced01-af-xdp/af-xdp-kern.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #include <linux/bpf.h>
-
 #include <bpf/bpf_helpers.h>
 
 struct bpf_map_def SEC("maps") xsks_map = {
@@ -11,39 +10,15 @@ struct bpf_map_def SEC("maps") xsks_map = {
         .max_entries = 64,  /* Assume netdev has no more than 64 queues */
 };
 
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-        .type        = BPF_MAP_TYPE_PERCPU_ARRAY,
-        .key_size    = sizeof(int),
-        .value_size  = sizeof(__u32),
-        .max_entries = 64,
-};
-
 SEC("xdp")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
     int index = ctx->rx_queue_index;
-//    __u32 *pkt_count;
-//
-//    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-//    if (pkt_count) {
-//        /* We pass every other packet 可是实测只有一开始的0才会被pass？ */
-//        if ((*pkt_count)++ & 1){ // 这里遗留一点问题：这种是怎么看的？
-//            return XDP_PASS;
-//        }
-//
-//    }
 
-    /* A set entry here means that the correspnding queue_id
-     * has an active AF_XDP socket bound to it. 也就是说：
-     * 只有绑定了运行着的AF_XDP套口的队列，才会继续下一步redirect……
-     * 所以必须上真实物理机了。
-     */
-    if (bpf_map_lookup_elem(&xsks_map, &index)){
-        //bpf_printk("We are here!\n");
+    if (bpf_map_lookup_elem(&xsks_map, &index)){ // 只有绑定了对应的XSK_fd才会redirect
         return bpf_redirect_map(&xsks_map, index, 0);
     }
-
-
+//    bpf_printk("No xsk bounded to queue %d\n", index);
     return XDP_PASS;
 }
 
